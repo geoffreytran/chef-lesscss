b15d07d0f2d2c4805933e30561337cf55a6f630b
Geoffrey Tran <geoffrey.tran@rapp.com>
2012-07-10 14:16:37 -0500

    Fixed depends

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ license          "Apache License, Version 2.0"
 description      "Installs/Configures lesscss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-depends          ["nodejs", "npm"]
+depends          "npm"
