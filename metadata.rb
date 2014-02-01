7555d10bc135e8d3d98a6189496e061d83a96ff0
Geoffrey Tran <geoffrey.tran@gmail.com>
2014-01-31 20:09:44 -0600

    Update version

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "geoffrey.tran@gmail.com"
 license          "Apache License, Version 2.0"
 description      "Installs/Configures lesscss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends          "npm"
